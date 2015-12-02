925a5c46a858abd6f32b9f2b4c94119d627d2870
Daniel Nguyen <danieltn91@gmail.com>
2015-12-02 14:05:48 -0800

    Update Ownership.java

--- GoodGame/src/java/gg/Ownership.java
+++ GoodGame/src/java/gg/Ownership.java
@@ -1,6 +1,47 @@
 
 package gg;
 
+import java.util.List;
+
 public class Ownership {
 	boolean isOwner = false;
+        String domain;
+        String name;
+  
+        private Party party; 
+        private Context context;
+        
+        public void doneBy(Party party)
+        {  
+            this.setParty(party); 
+        }   
+        public String getDomain() {
+            return domain; 
+        }  public void setDomain(String domain)
+        {  
+            this.domain = domain; 
+        }  public String getName() 
+        {   
+            return name;
+        }  public void setName(String name)
+        {  
+            this.name = name; 
+        }
+        public Context getContext()
+        {   
+            return context; 
+        }
+        public void setContext(Context context) 
+        {
+            this.context = context;
+        }
+        public Party getParty()
+        {
+            return party;
+        }
+        public void setParty(Party party) 
+        {
+            this.party = party;
+        }
 }
+
