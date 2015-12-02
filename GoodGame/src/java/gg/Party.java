eed5dc511f1204c7e187b46dd2a0bacffe6b1c25
Daniel Nguyen <danieltn91@gmail.com>
2015-12-01 22:06:16 -0800

    Update Party.java

--- GoodGame/src/java/gg/Party.java
+++ GoodGame/src/java/gg/Party.java
@@ -1,20 +1,26 @@
 
 package gg;
 
+//	A party is a group that is identified by a name and has licenses
+//	that gives them ownership to many things.
 public class Party {
-	public String name;
+	String name;
 	Evidence licenses;
+	private Intellect intellect;
+	private Property property;
 	
 	public Party(String name){
 		this.name = name;
 		licenses = new Evidence();
 	}
 	
+	// Claim ownership of something
 	public void addClaim(String claimName){
 		Claim cl = new Claim(claimName);
 		licenses.addGameLicense(cl);
 	}
 	
+	// Validate ownership
 	public boolean checkClaim(String gameTitle){
 		for (int i = 0; i < licenses.claims.size(); i++){
 			if (licenses.claims.get(i).name.equalsIgnoreCase(gameTitle)){
