0c15e5b8cb6c51a0ed528af91e1cb56c913c5418
Daniel Nguyen <danieltn91@gmail.com>
2015-12-01 22:03:41 -0800

    Update Context.java

--- GoodGame/src/java/gg/Context.java
+++ GoodGame/src/java/gg/Context.java
@@ -2,5 +2,20 @@
 package gg;
 
 public class Context {
-
+	private Type type;
+	private String contextType;
+	private List<String> relatedContext;
+	
+	public Context(String cType, List<String> rContext) {
+		contextType = ctype;
+		relatedContext = rContext;
+	}
+	
+	public String getContextType() {
+		return contextType;
+	}
+	
+	public List<String> getRelatedContext() {
+		return relatedContext;
+	}
 }
