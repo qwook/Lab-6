4ae6151b08b5a38a1dbca44ec4aeee48a8081889
Daniel Nguyen <danieltn91@gmail.com>
2015-12-01 22:04:50 -0800

    Update Event.java

--- GoodGame/src/java/gg/Event.java
+++ GoodGame/src/java/gg/Event.java
@@ -1,6 +1,42 @@
 
 package gg;
 
+import java.util.List;
 public class Event {
-
+	private List<Media> medias;
+	private String eventName;
+	private String eventLocation;
+	private ArrayList<String> attendees;
+	
+	public Event(String name, String location, ArrayList<String> people) {
+		eventName = name;
+		eventLocation = location;
+		attendeees = people;
+	}
+	
+	public String getEventName() {
+		return eventName;
+	}
+	
+	public String getEventLocation() {
+		return eventLocation;
+	}
+	
+	public ArrayList<String> getAttendees() {
+		return attendees;
+	}
+	public void recordedOn(List<Media> anyMedias) 
+	{   
+		this.medias = anyMedias;  
+	}
+	public List<Media> getMedias()
+	{   
+		return medias;
+	}
+	public void setMedias(List<Media> medias) 
+	{  
+		this.medias = medias;  
+		
+	}
+	
 }
