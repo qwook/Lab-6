1bb859dfc6ece2e15ce223d0781ecb5c723dc7c0
Daniel Nguyen <danieltn91@gmail.com>
2015-12-01 22:05:29 -0800

    Update Intellect.java

--- GoodGame/src/java/gg/Intellect.java
+++ GoodGame/src/java/gg/Intellect.java
@@ -1,5 +1,21 @@
 
 package gg;
 
+//	Intellect needs to be rated.
+//	Intellect must be considered intellectual or not
 public class Intellect {
+	private Evidence evidence;
+	private int IQ;
+	private boolean isIntellectual;
+	public Evidence generate()
+	{
+		return evidence;
+	}
+	public int getIQ() {
+		return IQ;
+	}
+	
+	public boolean isIntellectualEnough() {
+		return isIntellectual;
+	}
 }
