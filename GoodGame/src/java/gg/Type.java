e2998abf280a166bf15d6a223582ce6f9732bc39
Daniel Nguyen <danieltn91@gmail.com>
2015-12-01 22:06:54 -0800

    Update Type.java

--- GoodGame/src/java/gg/Type.java
+++ GoodGame/src/java/gg/Type.java
@@ -1,6 +1,34 @@
 
 package gg;
 
+import java.util.List;
+// A type is identified with a name and the context it exists within
 public class Type {
-
+	String name;
+	String ID;
+	Existence exist;
+	Context context;
+	private List<Event> event;
+	
+	public abstract void namesEvent(Event event);  
+	public void decideEvent(Event event)
+	{
+		this.event = event
+	}
+	public String getTypeName()
+	{  
+		return typeName;  
+	} 
+	public void setTypeName(String typeName)
+	{  
+		this.typeName = typeName; 
+	}
+	public String getID()
+	{
+		return ID;
+	}
+	public void setID(String ID)
+	{
+		this.ID = ID;
+	}
 }
